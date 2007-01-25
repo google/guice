70eb03318ccf5431f2292e1b7844aa148f6d38bd
crazyboblee <crazyboblee@d779f126-a31b-0410-b53b-1d3aecad763e>
2007-01-25 22:37:29 +0000

    Restored Spring performance comparison to working order.
    git-svn-id: https://google-guice.googlecode.com/svn/trunk@21 d779f126-a31b-0410-b53b-1d3aecad763e

--- test/com/google/inject/SpringPerformanceComparison.java
+++ test/com/google/inject/SpringPerformanceComparison.java
@@ -83,7 +83,6 @@ public class SpringPerformanceComparison {
       builder.bind("s").to("test");
 
       fooFactory = builder.create(false).getFactory(Key.get(Foo.class));
-      fooFactory.get();
     }
 
     public Foo call() throws Exception {
@@ -101,29 +100,25 @@ public class SpringPerformanceComparison {
   }
 
   public static void main(String[] args) throws Exception {
-    for (int i = 0; i < 10000000; i++) {
-      juiceFactory.call();
+    validate(springFactory);
+    validate(springFactory);
+    validate(juiceFactory);
+    validate(juiceFactory);
+
+    int count = 100000;
+    for (int i2 = 0; i2 < 10; i2++) {
+      long time = System.currentTimeMillis();
+      for (int i = 0; i < count; i++)
+        springFactory.call();
+      time = System.currentTimeMillis() - time;
+      System.err.println("Spring: " + count * 1000 / time + " creations/s");
+
+      time = System.currentTimeMillis();
+      for (int i = 0; i < count; i++)
+        juiceFactory.call();
+      time = System.currentTimeMillis() - time;
+      System.err.println("Guice:  " + count * 1000 / time + " creations/s");
     }
-//    validate(springFactory);
-//    validate(springFactory);
-//    validate(juiceFactory);
-//    validate(juiceFactory);
-//
-//    System.err.println("Creations of Foo per second:");
-//    int count = 100000;
-//    for (int i2 = 0; i2 < 10; i2++) {
-//      long time = System.currentTimeMillis();
-//      for (int i = 0; i < count; i++)
-//        springFactory.call();
-//      time = System.currentTimeMillis() - time;
-//      System.err.println("Spring: " + count * 1000 / time + "/s");
-//
-//      time = System.currentTimeMillis();
-//      for (int i = 0; i < count; i++)
-//        juiceFactory.call();
-//      time = System.currentTimeMillis() - time;
-//      System.err.println("Guice:  " + count * 1000 / time + "/s");
-//    }
   }
 
   public static class Foo {
