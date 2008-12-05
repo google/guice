afe0471c1f27c02c633585e6b16e295050ef80fa
limpbizkit <limpbizkit@d779f126-a31b-0410-b53b-1d3aecad763e>
2008-12-05 23:11:33 +0000

    Fixing a doc mistake, as pointed out by agertzen.
    We accidentally said 'better error checking' in the Javadoc for Stage.DEVELOPMENT. But all stages get the same error checking (and PRODUCTION gets errors eagerly).
    
    git-svn-id: https://google-guice.googlecode.com/svn/trunk@718 d779f126-a31b-0410-b53b-1d3aecad763e

--- src/com/google/inject/Stage.java
+++ src/com/google/inject/Stage.java
@@ -24,23 +24,21 @@ package com.google.inject;
 public enum Stage {
 
   /**
-   * We're running in a tool (an IDE plugin for example). We need binding
-   * meta data but not a functioning Injector. Do not inject members of
-   * instances. Do not load eager singletons. Do as little as possible so
-   * our tools run nice and snappy. Injectors created in this stage cannot
+   * We're running in a tool (an IDE plugin for example). We need binding meta data but not a
+   * functioning Injector. Do not inject members of instances. Do not load eager singletons. Do as
+   * little as possible so our tools run nice and snappy. Injectors created in this stage cannot
    * be used to satisfy injections.
    */
   TOOL,
 
   /**
-   * The default stage. We want fast startup times and better error reporting
-   * at the expense of runtime performance and some up front error checking.
+   * We want fast startup times at the expense of runtime performance and some up front error
+   * checking.
    */
   DEVELOPMENT,
 
   /**
-   * We want to catch errors as early as possible and take performance hits
-   * up front.
+   * We want to catch errors as early as possible and take performance hits up front.
    */
   PRODUCTION
 }
