6add9f6ce1a5e1bdacacc4389364670eb451de48
sberlin <sberlin@d779f126-a31b-0410-b53b-1d3aecad763e>
2010-02-22 01:30:52 +0000

    fixed InjectorBuilder method visibility, as pointed out by cgdecker in issue 395.
    git-svn-id: https://google-guice.googlecode.com/svn/trunk@1143 d779f126-a31b-0410-b53b-1d3aecad763e

--- src/com/google/inject/InjectorBuilder.java
+++ src/com/google/inject/InjectorBuilder.java
@@ -67,25 +67,25 @@ public class InjectorBuilder {
    * 
    * By default, explicit bindings are not required.
    */
-  InjectorBuilder requireExplicitBindings() {
+  public InjectorBuilder requireExplicitBindings() {
     this.jitDisabled = true;
     return this;
   }
 
   /** Adds more modules that will be used when the Injector is created. */
-  InjectorBuilder addModules(Iterable<? extends Module> modules) {
+  public InjectorBuilder addModules(Iterable<? extends Module> modules) {
     creator.addModules(modules);
     return this;
   }
   
   /** Adds more modules that will be used when the Injector is created. */
-  InjectorBuilder addModules(Module... modules) {
+  public InjectorBuilder addModules(Module... modules) {
     creator.addModules(Arrays.asList(modules));
     return this;
   }
 
   /** Builds the injector. */
-  Injector build() {
+  public Injector build() {
     creator.injectorOptions(new InternalInjectorCreator.InjectorOptions(stage, jitDisabled));
     return creator.build();
   }
