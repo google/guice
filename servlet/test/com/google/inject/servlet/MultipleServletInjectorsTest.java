298db67e29724632918ee660f520cd33a467ce50
dhanji <dhanji@d779f126-a31b-0410-b53b-1d3aecad763e>
2010-07-06 02:06:36 +0000

    limpbizkit review fixes for r1187
    git-svn-id: https://google-guice.googlecode.com/svn/trunk@1188 d779f126-a31b-0410-b53b-1d3aecad763e

--- servlet/test/com/google/inject/servlet/MultipleServletInjectorsTest.java
+++ servlet/test/com/google/inject/servlet/MultipleServletInjectorsTest.java
@@ -1,5 +1,18 @@
-// Copyright 2010 Google Inc. All Rights Reserved.
-
+/**
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.inject.servlet;
 
 import com.google.inject.Guice;
@@ -21,7 +34,7 @@ import static org.easymock.EasyMock.verify;
  * This gorgeous test asserts that multiple servlet pipelines can
  * run in the SAME JVM. booya.
  *
- * @author dhanji@google.com (Dhanji R. Prasanna)
+ * @author dhanji@gmail.com (Dhanji R. Prasanna)
  */
 public class MultipleServletInjectorsTest extends TestCase {
 
