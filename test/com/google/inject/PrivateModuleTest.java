75ae834b54eaf6d12712af50612ec9dc81ec8053
limpbizkit@gmail.com <limpbizkit@gmail.com@d779f126-a31b-0410-b53b-1d3aecad763e>
2010-08-26 19:01:53 +0000

    Changing Guice Javadoc to build with Doclava (beta)
    git-svn-id: https://google-guice.googlecode.com/svn/trunk@1209 d779f126-a31b-0410-b53b-1d3aecad763e

--- test/com/google/inject/PrivateModuleTest.java
+++ test/com/google/inject/PrivateModuleTest.java
@@ -22,8 +22,11 @@ import com.google.inject.name.Named;
 import com.google.inject.name.Names;
 import static com.google.inject.name.Names.named;
 import com.google.inject.spi.Dependency;
+import com.google.inject.spi.Element;
+import com.google.inject.spi.Elements;
 import com.google.inject.spi.ExposedBinding;
 import com.google.inject.spi.PrivateElements;
+import com.google.inject.util.ParallelModule;
 import junit.framework.TestCase;
 
 /**
