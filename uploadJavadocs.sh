f0471274eb9de40f6ef99b2f8e909b2d1b803480
limpbizkit@gmail.com <limpbizkit@gmail.com@d779f126-a31b-0410-b53b-1d3aecad763e>
2010-10-05 03:07:31 +0000

    Removed old Javadocs.
    git-svn-id: https://google-guice.googlecode.com/svn/trunk@1284 d779f126-a31b-0410-b53b-1d3aecad763e

--- uploadJavadocs.sh
+++ uploadJavadocs.sh
@@ -1,6 +1,6 @@
 svn rm latest-javadoc
 svn ci -m "Removed old Javadocs."
 ant javadoc
-mv build/javadoc latest-javadoc
+mv build/docs/javadoc latest-javadoc
 svn add latest-javadoc
 svn ci -m "Added updated Javadocs."
