b0f00226b7e1008bbcf8267fe7000c66e2230db2
sberlin@gmail.com <sberlin@gmail.com@d779f126-a31b-0410-b53b-1d3aecad763e>
2010-10-30 14:39:42 +0000

    Added updated Javadocs.
    git-svn-id: https://google-guice.googlecode.com/svn/trunk@1330 d779f126-a31b-0410-b53b-1d3aecad763e

--- uploadJavadocs.sh
+++ uploadJavadocs.sh
@@ -1,8 +1,7 @@
-#svn rm latest-javadoc javadoc
-rm -rf latest-javadoc javadoc
-#svn ci -m "Removed old Javadocs."
+svn rm latest-javadoc javadoc
+svn ci -m "Removed old Javadocs."
 ant javadoc
 mv build/docs/javadoc latest-javadoc
 cp -r latest-javadoc javadoc
-#svn add latest-javadoc javadoc
-#svn ci -m "Added updated Javadocs."
+svn add latest-javadoc javadoc
+svn ci -m "Added updated Javadocs."
