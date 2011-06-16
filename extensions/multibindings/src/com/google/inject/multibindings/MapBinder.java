6cf0488ecfd2dc6acaa9ee78fe1b57d1bc07537f
drew.mclaughlin <drew.mclaughlin@d779f126-a31b-0410-b53b-1d3aecad763e>
2011-06-16 20:02:56 +0000

    Remove unnecessary import added in r1555
    git-svn-id: https://google-guice.googlecode.com/svn/trunk@1556 d779f126-a31b-0410-b53b-1d3aecad763e

--- extensions/multibindings/src/com/google/inject/multibindings/MapBinder.java
+++ extensions/multibindings/src/com/google/inject/multibindings/MapBinder.java
@@ -44,7 +44,6 @@ import com.google.inject.spi.Toolable;
 import com.google.inject.util.Types;
 import static com.google.inject.util.Types.newParameterizedTypeWithOwner;
 import java.lang.annotation.Annotation;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
