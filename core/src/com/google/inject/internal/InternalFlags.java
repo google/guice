e1197a962b656e5128cfa01319e42db19c8ffc4b
Christian Edward Gruber <cgruber@google.com>
2013-11-12 23:06:31 -0800

    Update Guice's maven build system to run the unit tests in three configurations, representing the three different values for stack-trace inclusion in error messages.  Additionally, modify the open-source InternalFlags to log a warning if a value is passed to the flag that does not correspond to the Enum.
    Tested:
    normal open-source tests in google's internal build
    full maven build from MOE's assembly of the open-source project
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=54815820

--- core/src/com/google/inject/internal/InternalFlags.java
+++ core/src/com/google/inject/internal/InternalFlags.java
@@ -1,33 +1,36 @@
 package com.google.inject.internal;
 
-
+import java.util.Arrays;
+import java.util.logging.Logger;
 /**
  * Contains flags for Guice.
  */
 public class InternalFlags {
-
+  private final static Logger logger = Logger.getLogger(InternalFlags.class.getName());
   /**
    * The options for Guice stack trace collection.
    */
   public enum IncludeStackTraceOption {
-    // No stack trace collection
+    /** No stack trace collection */
     OFF,
-    // Minimum stack trace collection (Default)
+    /**  Minimum stack trace collection (Default) */
     ONLY_FOR_DECLARING_SOURCE,
-    // Full stack trace for everything
+    /** Full stack trace for everything */
     COMPLETE
   }
 
 
   public static IncludeStackTraceOption getIncludeStackTraceOption() {
-    String propertyValue = System.getProperty(
-        "guice_include_stack_traces");
-    if (IncludeStackTraceOption.OFF.name().equals(propertyValue)) {
-        return IncludeStackTraceOption.OFF;
+    String flag = System.getProperty("guice_include_stack_traces");
+    try {
+      return (flag == null || flag.length() == 0)
+          ? IncludeStackTraceOption.ONLY_FOR_DECLARING_SOURCE
+          : IncludeStackTraceOption.valueOf(flag);
+    } catch (IllegalArgumentException e) {
+      logger.warning(flag
+          + " is not a valid flag value for guice_include_stack_traces. "
+          + " Values must be one of " + Arrays.asList(IncludeStackTraceOption.values()));
+      return IncludeStackTraceOption.ONLY_FOR_DECLARING_SOURCE;
     }
-    if (IncludeStackTraceOption.COMPLETE.name().equals(propertyValue)) {
-        return IncludeStackTraceOption.COMPLETE;
-    }
-    return IncludeStackTraceOption.ONLY_FOR_DECLARING_SOURCE;
   }
 }
