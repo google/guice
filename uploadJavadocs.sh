016d079f89d9840f882827c338fba39399151b7e
Christian Edward Gruber <cgruber@google.com>
2014-03-20 20:09:52 -0700

    Fix uploadJavadocs.sh script to work with git.

--- uploadJavadocs.sh
+++ uploadJavadocs.sh
@@ -1,9 +1,9 @@
 rm -rf build/docs
-svn rm latest-javadoc javadoc
-svn ci -m "Removed old Javadocs." latest-javadoc javadoc
+git rm -r latest-javadoc javadoc
 ant javadoc
 cp -r build/docs/javadoc latest-javadoc
 cp -r latest-javadoc javadoc
 mv build/docs/guice-*.xml lib/build
-svn add latest-javadoc javadoc lib/build/guice-*.xml
-svn ci -m "Added updated Javadocs." latest-javadoc javadoc lib/build/guice-*.xml
+git add -f -A latest-javadoc javadoc lib/build/guice-*.xml
+git commit -m "Added updated Javadocs." latest-javadoc javadoc lib/build/guice-*.xml
+
