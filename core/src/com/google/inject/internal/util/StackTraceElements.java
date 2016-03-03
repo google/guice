fc17191f9213131d1d7d3d372caf984feaed84a9
kak <kak@google.com>
2016-03-03 14:40:09 -0800

    Split out String and InMemoryStackTraceElement caches to avoid unsafe casts. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=116294578

--- core/src/com/google/inject/internal/util/StackTraceElements.java
+++ core/src/com/google/inject/internal/util/StackTraceElements.java
@@ -23,8 +23,8 @@ import com.google.common.cache.LoadingCache;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Member;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Creates stack trace elements for members.
@@ -52,7 +52,11 @@ public class StackTraceElements {
           });
   /*end[AOP]*/
 
-  private static Map<Object, Object> cache = new ConcurrentHashMap<Object, Object>();
+  private static final ConcurrentMap<InMemoryStackTraceElement, InMemoryStackTraceElement>
+      elementCache = new ConcurrentHashMap<InMemoryStackTraceElement, InMemoryStackTraceElement>();
+  private static final ConcurrentMap<String, String> stringCache =
+      new ConcurrentHashMap<String, String>();
+
   private static final String UNKNOWN_SOURCE = "Unknown Source";
 
   public static Object forMember(Member member) {
@@ -96,7 +100,8 @@ public class StackTraceElements {
    * Clears the internal cache for {@link StackTraceElement StackTraceElements}.
    */
   public static void clearCache() {
-    cache.clear();
+    elementCache.clear();
+    stringCache.clear();
   }
   
   /**
@@ -138,8 +143,7 @@ public class StackTraceElements {
   
   private static InMemoryStackTraceElement weakIntern(
       InMemoryStackTraceElement inMemoryStackTraceElement) {
-    InMemoryStackTraceElement cached = 
-        (InMemoryStackTraceElement) cache.get(inMemoryStackTraceElement);
+    InMemoryStackTraceElement cached = elementCache.get(inMemoryStackTraceElement);
     if (cached != null) {
       return cached;
     }
@@ -147,17 +151,17 @@ public class StackTraceElements {
         weakIntern(inMemoryStackTraceElement.getClassName()), 
         weakIntern(inMemoryStackTraceElement.getMethodName()), 
         inMemoryStackTraceElement.getLineNumber());
-    cache.put(inMemoryStackTraceElement, inMemoryStackTraceElement);
+    elementCache.put(inMemoryStackTraceElement, inMemoryStackTraceElement);
     return inMemoryStackTraceElement;
   }
   
   private static String weakIntern(String s) {
-    String cached = (String) cache.get(s);
+    String cached = stringCache.get(s);
     if (cached != null) {
       return cached;
     }
-    cache.put(s, s);
-    return s;  
+    stringCache.put(s, s);
+    return s;
   }
   
   /**
