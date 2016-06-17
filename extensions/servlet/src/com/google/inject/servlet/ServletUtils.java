fdb604c2928ebbdf0ed828578bda6c50133ce9ee
Sam Berlin <sameb@google.com>
2016-06-17 13:14:29 -0400

    switch from StandardCharsets to guava's Charsets, since the former is java7+.

--- extensions/servlet/src/com/google/inject/servlet/ServletUtils.java
+++ extensions/servlet/src/com/google/inject/servlet/ServletUtils.java
@@ -16,8 +16,8 @@
 
 package com.google.inject.servlet;
 
+import static com.google.common.base.Charsets.UTF_8;
 import static com.google.common.base.Preconditions.checkNotNull;
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
