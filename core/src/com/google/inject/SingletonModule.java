f75c6414159c08875b19d5feba172350e46ba2ec
mveitas <mveitas@gmail.com>
2017-09-19 08:39:20 -0400

    Fixing indentation from 4 to 2 spaces to match project

--- core/src/com/google/inject/SingletonModule.java
+++ core/src/com/google/inject/SingletonModule.java
@@ -2,22 +2,22 @@ package com.google.inject;
 
 public abstract class SingletonModule extends AbstractModule {
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-
-        if (!(obj instanceof AbstractModule)) {
-            return false;
-        }
-
-        return getClass().equals(obj.getClass());
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
     }
 
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
+    if (!(obj instanceof AbstractModule)) {
+      return false;
     }
+
+    return getClass().equals(obj.getClass());
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
 
 }
--- core/test/com/google/inject/SingletonModuleTest.java
+++ core/test/com/google/inject/SingletonModuleTest.java
@@ -4,34 +4,34 @@ import junit.framework.TestCase;
 
 public class SingletonModuleTest extends TestCase {
 
-    static int counter = 0;
+  static int counter = 0;
 
+  @Override
+  public void setUp() throws Exception {
+    counter = 0;
+  }
+
+  public void testInstallBaseModuleOnlyOnce() {
+    Guice.createInjector(new ModuleOne(), new ModuleOne());
+    assertEquals(1, counter);
+  }
+
+  public void testInstallAbstractModuleMoreThanOnce() {
+    Guice.createInjector(new ModuleTwo(), new ModuleTwo());
+    assertEquals(2, counter);
+  }
+
+  static class ModuleOne extends SingletonModule {
     @Override
-    public void setUp() throws Exception {
-        counter = 0;
+    protected void configure() {
+      counter++;
     }
+  }
 
-    public void testInstallBaseModuleOnlyOnce() {
-        Guice.createInjector(new ModuleOne(), new ModuleOne());
-        assertEquals(1, counter);
+  static class ModuleTwo extends AbstractModule {
+    @Override
+    protected void configure() {
+      counter++;
     }
-
-    public void testInstallAbstractModuleMoreThanOnce() {
-        Guice.createInjector(new ModuleTwo(), new ModuleTwo());
-        assertEquals(2, counter);
-    }
-
-    static class ModuleOne extends SingletonModule {
-        @Override
-        protected void configure() {
-            counter++;
-        }
-    }
-
-    static class ModuleTwo extends AbstractModule {
-        @Override
-        protected void configure() {
-            counter++;
-        }
-    }
+  }
 }
