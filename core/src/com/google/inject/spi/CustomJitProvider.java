7dc8ef48697b3847e6e71cab116b25096854b223
Pavel Shirshov <pshirshov@gmail.com>
2017-09-21 17:35:24 +0100

    #1128 docstring

--- core/src/com/google/inject/spi/CustomJitProvider.java
+++ core/src/com/google/inject/spi/CustomJitProvider.java
@@ -4,10 +4,19 @@ import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.internal.Errors;
 import com.google.inject.internal.ErrorsException;
-import com.google.inject.spi.Dependency;
 
+/**
+ * This interface allows to hijack Just-In-Time bindings logic.
+ *
+ * All the implementations of this interface which are present in current injector
+ * are going to be queried (in unpredictable order) for an instance of any JIT binding
+ * before any other lookups.
+ *
+ * @author pshirshov@gmail.com (Pavel Shirshov)
+ * @since 4.1.1
+ */
 public interface CustomJitProvider {
   <T> boolean supports(Key<T> key, Injector injector);
 
-  <T> T provide(Key<T> key, Injector internalFactory, Errors errors, Dependency<?> dependency) throws ErrorsException;
+  <T> T provide(Key<T> key, Injector injector, Errors errors, Dependency<?> dependency) throws ErrorsException;
 }
