cd875c4b918126271252446cf6a852f80526d499
dpb <dpb@google.com>
2017-10-02 14:46:39 -0700

    Use ServiceLoader, not ServiceRegistry. In Java 9, ServiceRegistry is restricted to ImageIO SPI types.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=170756352

--- core/test/com/googlecode/guice/OSGiContainerTest.java
+++ core/test/com/googlecode/guice/OSGiContainerTest.java
@@ -23,9 +23,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Iterator;
 import java.util.Properties;
-import javax.imageio.spi.ServiceRegistry;
+import java.util.ServiceLoader;
 import junit.framework.TestCase;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.launch.Framework;
@@ -136,9 +135,8 @@ public class OSGiContainerTest extends TestCase {
     properties.setProperty("org.osgi.framework.storage.clean", "onFirstInit");
 
     // test each available OSGi framework in turn
-    Iterator<FrameworkFactory> f = ServiceRegistry.lookupProviders(FrameworkFactory.class);
-    while (f.hasNext()) {
-      Framework framework = f.next().newFramework(properties);
+    for (FrameworkFactory frameworkFactory : ServiceLoader.load(FrameworkFactory.class)) {
+      Framework framework = frameworkFactory.newFramework(properties);
 
       framework.start();
       BundleContext systemContext = framework.getBundleContext();
