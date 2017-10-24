288945506177cba92ae7594749879dae1137596a
lukes <lukes@google.com>
2017-10-24 14:53:24 -0700

    Further define the ordering of injection points returned by InjectionPoint.forInstanceMethodsAndField and InjectionPoint.forStaticMethodsAndFields
    Previously Guice defined the order between fields and methods and between super types and subtypes but failed to define the order of injection points with the set of fields or methods of a single class.  This is problematic because Class.getDeclaredMethods and Class.getDeclaredFields return their elements in an undefined order.  Additionally, some JVMs actually randomize the order from run to run.  In this cl we define a consistent order by sorting, but we do not define the order.
    
    This is a prefactoring to change the way provisioning errors are reported.  Currently guice will collect multiple errors during provisioning and report all of them but it is changing to only report the 'first' one.  Without this change the error reported by multiple failing member injections will become non-deterministic.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=173315505

--- core/test/com/google/inject/spi/InjectionPointTest.java
+++ core/test/com/google/inject/spi/InjectionPointTest.java
@@ -17,6 +17,7 @@
 package com.google.inject.spi;
 
 import static com.google.common.collect.Iterables.getOnlyElement;
+import static com.google.common.truth.Truth.assertThat;
 import static com.google.inject.Asserts.assertContains;
 import static com.google.inject.Asserts.assertEqualsBothWays;
 import static com.google.inject.Asserts.assertNotSerializable;
@@ -197,26 +198,34 @@ public class InjectionPointTest extends TestCase {
   public void testAddForInstanceMethodsAndFields() throws Exception {
     Method instanceMethod = HasInjections.class.getMethod("instanceMethod", String.class);
     Field instanceField = HasInjections.class.getField("instanceField");
+    Field instanceField2 = HasInjections.class.getField("instanceField2");
 
     TypeLiteral<HasInjections> type = TypeLiteral.get(HasInjections.class);
-    assertEquals(
-        ImmutableSet.of(
-            new InjectionPoint(type, instanceMethod, false),
-            new InjectionPoint(type, instanceField, false)),
-        InjectionPoint.forInstanceMethodsAndFields(HasInjections.class));
+    Set<InjectionPoint> injectionPoints =
+        InjectionPoint.forInstanceMethodsAndFields(HasInjections.class);
+    // there is a defined order. assert on it
+    assertThat(injectionPoints)
+        .containsExactly(
+            new InjectionPoint(type, instanceField, false),
+            new InjectionPoint(type, instanceField2, false),
+            new InjectionPoint(type, instanceMethod, false))
+        .inOrder();
   }
 
   public void testAddForStaticMethodsAndFields() throws Exception {
     Method staticMethod = HasInjections.class.getMethod("staticMethod", String.class);
     Field staticField = HasInjections.class.getField("staticField");
+    Field staticField2 = HasInjections.class.getField("staticField2");
 
     Set<InjectionPoint> injectionPoints =
         InjectionPoint.forStaticMethodsAndFields(HasInjections.class);
-    assertEquals(
-        ImmutableSet.of(
-            new InjectionPoint(TypeLiteral.get(HasInjections.class), staticMethod, false),
-            new InjectionPoint(TypeLiteral.get(HasInjections.class), staticField, false)),
-        injectionPoints);
+    TypeLiteral<HasInjections> type = TypeLiteral.get(HasInjections.class);
+    assertThat(injectionPoints)
+        .containsExactly(
+            new InjectionPoint(type, staticField, false),
+            new InjectionPoint(type, staticField2, false),
+            new InjectionPoint(type, staticMethod, false))
+        .inOrder();
   }
 
   static class HasInjections {
@@ -228,11 +237,19 @@ public class InjectionPointTest extends TestCase {
     public static String staticField;
 
     @Inject
+    @Named("c")
+    public static String staticField2;
+
+    @Inject
     public void instanceMethod(@Named("d") String d) {}
 
     @Inject
     @Named("f")
     public String instanceField;
+
+    @Inject
+    @Named("f")
+    public String instanceField2;
   }
 
   public void testAddForParameterizedInjections() {
