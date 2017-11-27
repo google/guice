01b0f294eecba55884fb5dc2123b262f25ce44c5
cushon <cushon@google.com>
2017-11-27 09:50:46 -0800

    Prepare code using ASM for Java 9
    by updating to the ASM 6 API level, which enables support for v52 class files.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=177030674

--- core/src/com/google/inject/internal/util/LineNumbers.java
+++ core/src/com/google/inject/internal/util/LineNumbers.java
@@ -138,7 +138,7 @@ final class LineNumbers {
     private String name;
 
     LineNumberReader() {
-      super(Opcodes.ASM5);
+      super(Opcodes.ASM6);
     }
 
     @Override
@@ -197,7 +197,7 @@ final class LineNumbers {
 
     class LineNumberMethodVisitor extends MethodVisitor {
       LineNumberMethodVisitor() {
-        super(Opcodes.ASM5);
+        super(Opcodes.ASM6);
       }
 
       @Override
@@ -228,7 +228,7 @@ final class LineNumbers {
 
     class LineNumberAnnotationVisitor extends AnnotationVisitor {
       LineNumberAnnotationVisitor() {
-        super(Opcodes.ASM5);
+        super(Opcodes.ASM6);
       }
 
       @Override
