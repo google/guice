9d1fb7c805ca4720b6e21190185a974aa268378c
bhagwani <bhagwani@google.com>
2018-10-22 09:44:03 -0700

    Suppress warnings in classes that implement equals() without also implementing hashCode().
    The contract for Object.hashCode states that if two objects are equal, then
    calling the hashCode() method on each of the two objects must produce the same
    result. Implementing equals() but not hashCode() causes broken behaviour when
    trying to store the object in a collection.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=218182747

--- core/test/com/google/inject/DuplicateBindingsTest.java
+++ core/test/com/google/inject/DuplicateBindingsTest.java
@@ -504,7 +504,7 @@ public class DuplicateBindingsTest extends TestCase {
     }
 
     @Override
-    @SuppressWarnings("EqualsBrokenForNull") // intentionally NPE on null for the test
+    @SuppressWarnings({"EqualsHashCode", "EqualsBrokenForNull"}) // intentionally NPE on null for the test
     public boolean equals(Object obj) {
       return obj.getClass() == getClass();
     }
