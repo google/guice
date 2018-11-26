c0efb27303925c9944bc7f0ec97120494bcccd4f
GedMarc <ged_marc@hotmail.com>
2018-11-27 00:18:45 +0200

    Allow CgLib through to Proxy, Reflect, and Intercept

--- core/moditect_no_aop/module-info.java
+++ core/moditect_no_aop/module-info.java
@@ -6,6 +6,10 @@ module com.google.guice {
 	exports com.google.inject.spi;
 	exports com.google.inject.multibindings;
 
+	exports com.google.inject.internal.cglib.core;
+	exports com.google.inject.internal.cglib.proxy;
+	exports com.google.inject.internal.cglib.reflect;
+
 	exports com.google.inject.internal to com.google.guice.extensions.throwingproviders, com.google.guice.extensions.testlib, com.google.guice.extensions.servlet, com.google.guice.extensions.struts2, com.google.guice.extensions.grapher, com.google.guice.extensions.daggeradapter, com.google.guice.extensions.assistedinject;
 	exports com.google.inject.util to com.google.guice.extensions.throwingproviders, com.google.guice.extensions.testlib, com.google.guice.extensions.assistedinject;
 	exports com.google.inject.internal.util to com.google.guice.extensions.throwingproviders, com.google.guice.extensions.grapher, com.google.guice.extensions.assistedinject;
