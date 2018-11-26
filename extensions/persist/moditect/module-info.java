c1be62e43c7757943034308ffb4bfc4cab8b2647
GedMarc <ged_marc@hotmail.com>
2018-11-26 14:25:58 +0200

    JPMS Guice Travis Builds

--- extensions/persist/moditect/module-info.java
+++ extensions/persist/moditect/module-info.java
@@ -2,9 +2,10 @@ module com.google.guice.extensions.persist {
 	exports com.google.inject.persist;
 
 	requires transitive com.google.guice;
+	requires com.google.common;
 
 	requires static javax.servlet.api;
-	requires aopalliance;
-	requires hibernate.jpa;
-	requires com.google.common;
+	requires static aopalliance;
+	requires static hibernate.jpa;
+
 }
