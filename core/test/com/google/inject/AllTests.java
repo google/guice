5a69da0b6a3f087decf2a671205b584c92c79c7f
vzm <vzm@google.com>
2020-06-02 02:26:29 -0700

    Upgrade ModuleAnnotatedMethodScanner test to JUnit4 in preparation for some changes to scanner behavior.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=314292845

--- core/test/com/google/inject/AllTests.java
+++ core/test/com/google/inject/AllTests.java
@@ -31,7 +31,6 @@ import com.google.inject.spi.HasDependenciesTest;
 import com.google.inject.spi.InjectionPointTest;
 import com.google.inject.spi.InjectorSpiTest;
 import com.google.inject.spi.MessageTest;
-import com.google.inject.spi.ModuleAnnotatedMethodScannerTest;
 import com.google.inject.spi.ModuleRewriterTest;
 import com.google.inject.spi.ModuleSourceTest;
 import com.google.inject.spi.ProviderMethodsTest;
@@ -76,7 +75,6 @@ public class AllTests {
     suite.addTestSuite(MembersInjectorTest.class);
     suite.addTestSuite(ModulesTest.class);
     suite.addTestSuite(ModuleTest.class);
-    suite.addTestSuite(ModuleAnnotatedMethodScannerTest.class);
     suite.addTestSuite(NullableInjectionPointTest.class);
     suite.addTestSuite(OptionalBindingTest.class);
     suite.addTestSuite(OverrideModuleTest.class);
