3c5e88f102dd8fd9d19bdca8983bdcacc9213a8c
xiaomingjia <xiaomingjia@google.com>
2020-08-14 14:53:11 -0700

    Only display learn more link for documented error id.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=326734989

--- core/src/com/google/inject/internal/InternalErrorDetail.java
+++ core/src/com/google/inject/internal/InternalErrorDetail.java
@@ -1,6 +1,7 @@
 package com.google.inject.internal;
 
 import com.google.common.base.CaseFormat;
+import com.google.common.collect.ImmutableSet;
 import com.google.inject.spi.ErrorDetail;
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +11,27 @@ import java.util.Optional;
  * application code.
  */
 abstract class InternalErrorDetail<T extends ErrorDetail<T>> extends ErrorDetail<T> {
+  // A list of errors that have help documentation.
+  private static final ImmutableSet<ErrorId> DOCUMENTED_ERRORS =
+      ImmutableSet.<ErrorId>builder()
+          .add(ErrorId.BINDING_ALREADY_SET)
+          .add(ErrorId.CAN_NOT_PROXY_CLASS)
+          .add(ErrorId.CIRCULAR_PROXY_DISABLED)
+          .add(ErrorId.DUPLICATE_BINDING_ANNOTATIONS)
+          .add(ErrorId.DUPLICATE_ELEMENT)
+          .add(ErrorId.DUPLICATE_SCOPES)
+          .add(ErrorId.ERROR_INJECTING_CONSTRUCTOR)
+          .add(ErrorId.ERROR_INJECTING_METHOD)
+          .add(ErrorId.ERROR_IN_CUSTOM_PROVIDER)
+          .add(ErrorId.INJECT_INNER_CLASS)
+          .add(ErrorId.MISSING_CONSTRUCTOR)
+          .add(ErrorId.MISSING_IMPLEMENTATION)
+          .add(ErrorId.NULL_INJECTED_INTO_NON_NULLABLE)
+          .add(ErrorId.NULL_VALUE_IN_MAP)
+          .add(ErrorId.SCOPE_NOT_FOUND)
+          .add(ErrorId.TOO_MANY_CONSTRUCTORS)
+          .build();
+
   private static final String DOC_BASE_URL = "https://github.com/googel/guice/wiki";
 
   protected final ErrorId errorId;
@@ -27,6 +49,9 @@ abstract class InternalErrorDetail<T extends ErrorDetail<T>> extends ErrorDetail
 
   @Override
   protected final Optional<String> getLearnMoreLink() {
-    return Optional.of(String.format("%s/%s", DOC_BASE_URL, errorId.name()));
+    if (DOCUMENTED_ERRORS.contains(errorId)) {
+      return Optional.of(String.format("%s/%s", DOC_BASE_URL, errorId.name()));
+    }
+    return Optional.empty();
   }
 }
