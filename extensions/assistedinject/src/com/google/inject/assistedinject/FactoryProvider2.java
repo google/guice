67a5327dc0c74da36711df64a42f1c0652bb1d7a
xiaomingjia <xiaomingjia@google.com>
2020-08-14 14:31:40 -0700

    Delete TODO to make `@Inject` required to use assisted inject.
    Since it's actually possible to use assisted inject with field or method injection, an `@Inject` annotated constructor is not necessary.
    
    This does mean that assisted injection does not respect the `binder().requireAtInjectOnConstructors()` requirement and it is non-trivial to propagate the injector option down to enforce this because the injection point is looked up at module configuration time.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=326730924

--- extensions/assistedinject/src/com/google/inject/assistedinject/FactoryProvider2.java
+++ extensions/assistedinject/src/com/google/inject/assistedinject/FactoryProvider2.java
@@ -545,11 +545,8 @@ final class FactoryProvider2<F>
     }
 
     if (!anyAssistedInjectConstructors) {
-      // If none existed, use @Inject.
+      // If none existed, use @Inject or a no-arg constructor.
       try {
-        // TODO(b/151482394): Change this to enforce that there is a @Inject annotated cosntructor
-        // since it doesn't make sense to use assisted inject with a no-arg constructor, regardless
-        // if the injector is configured to require @Inject annotation or not.
         return InjectionPoint.forConstructorOf(implementation);
       } catch (ConfigurationException e) {
         errors.merge(e.getErrorMessages());
