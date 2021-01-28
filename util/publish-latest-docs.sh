62276f87be2cbb742bc249d683b44b7c159d47c0
Guice Team <java-team-github-bot@google.com>
2021-01-28 11:50:28 -0800

    Configure github actions to publish latest javadoc and snapshot after a success test run.
    PiperOrigin-RevId: 354360420

--- util/publish-latest-docs.sh
+++ util/publish-latest-docs.sh
@@ -1,24 +1,21 @@
-if [ "$TRAVIS_REPO_SLUG" == "google/guice" ] && \
-   [ "$TRAVIS_JDK_VERSION" == "openjdk11" ] && \
-   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
-   [ "$TRAVIS_BRANCH" == "master" ]; then
-  bash -e util/generate-latest-docs.sh
-  echo -e "Publishing javadoc & JDiff...\n"
-  mkdir -p $HOME/guice-docs/latest
-  cp -R build/docs/* $HOME/guice-docs/latest/
+#!/bin/bash
 
-  cd $HOME
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/google/guice gh-pages > /dev/null
+bash -e util/generate-latest-docs.sh
+echo -e "Publishing javadoc & JDiff...\n"
+mkdir -p $HOME/guice-docs/latest
+cp -R build/docs/* $HOME/guice-docs/latest/
 
-  cd gh-pages
-  git rm -rf api-docs/latest
-  mkdir -p api-docs/latest
-  cp -rf $HOME/guice-docs/latest/* api-docs/latest/
-  git add -f .
-  git commit -m "Latest javadoc & api-diffs on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+cd $HOME
+git config --global user.email "guice-dev+github@google.com"
+git config --global user.name "guice-dev+github"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/google/guice gh-pages > /dev/null
 
-  echo -e "Published Javadoc & JDiff to gh-pages.\n"
-fi
+cd gh-pages
+git rm -rf api-docs/latest
+mkdir -p api-docs/latest
+cp -rf $HOME/guice-docs/latest/* api-docs/latest/
+git add -f .
+git commit -m "Latest javadoc & api-diffs on successful CI build $GITHUB_SHA auto-pushed to gh-pages"
+git push -fq origin gh-pages > /dev/null
+
+echo -e "Published Javadoc & JDiff to gh-pages.\n"
