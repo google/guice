f78f7cfb3b7cb336e8bc8ad7fe421b24805720e9
Guice Team <java-team-github-bot@google.com>
2021-02-03 16:24:54 -0800

    Temporarily down grade the warning to INFO level.
    The warning is causing some test failures due to unexpected log messages.
    
    PiperOrigin-RevId: 355507369

--- extensions/assistedinject/src/com/google/inject/assistedinject/FactoryProvider2.java
+++ extensions/assistedinject/src/com/google/inject/assistedinject/FactoryProvider2.java
@@ -377,8 +377,9 @@ final class FactoryProvider2<F>
             && userLookups == null
             && !Modifier.isPublic(factory.getClass().getModifiers())) {
           warnedAboutUserLookups = true;
+          // TODO(b/179303203): Upgrade this back to WARNING level
           logger.log(
-              Level.WARNING,
+              Level.INFO,
               "AssistedInject factory {0} is non-public and has javac-generated default methods. "
                   + " Please pass a `MethodHandles.lookups()` with"
                   + " FactoryModuleBuilder.withLookups when using this factory so that Guice can"
