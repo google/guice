af40f721c002e3cc040e2d49124264ab37e828ea
unknown <wendywang647@gmail.com>
2021-05-25 09:33:12 -0700

    StatsObject

--- guice-benchmark/src/benchmark/StatsObject.java
+++ guice-benchmark/src/benchmark/StatsObject.java
@@ -15,19 +15,22 @@ public class StatsObject {
 
 	private List<Long> timings; // needs to be sorted at some point before stats is given
 	private String className;
+	private long max;
+	private long min;
+	private long sum;
 
 	/**
 	 * 
 	 * @param className
 	 */
 	public StatsObject(String className) {
-		this.timings = new ArrayList<>();
+		this.timings = new ArrayList<Long>();
 		this.className = className;
 	}
-
-		/**
+	
+	/**
 	 * 
-	 * @return
+	 * @return className of the class whose injected durations are managed by this StatsObject
 	 */
 	public String getClassName() {
 		return className;
@@ -38,20 +41,46 @@ public class StatsObject {
 	 * @param time
 	 */
 	public void addTiming(long time) {
-		// add time
-		// sort list
+		// add the incoming new time into the list
+		timings.add(time);
+		
+		// update the max and min if necessary
+		if(time < min)
+			min = time;
+		if(time > max)
+			max = time;
+		
+		// update sum
+		sum += time;
 	}
 
 	/**
 	 * @return the average
 	 */
 	public long getAverage() {
-		return 0;
+		// use sum to calculate average
+		long average = 0;
+		if(!timings.isEmpty())
+			average = sum / timings.size();
+		
+		return average;
 	}
 
-	// give worst time
+	/**
+	 * Gives the worst time
+	 * @return the max duration in the timings list
+	 */
+	public long getMax() {
+		return max;
+	}
 
-	// give best time
+	/**
+	 * Gives the best time
+	 * @return the min duration in the timing list
+	 */
+	public long getMin() {
+		return min;
+	}
 
 	// give other stats?
 }
