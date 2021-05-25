00129668c75e3b6e0d88d31c1f3c666c5aed009e
Meghna Reddy <meghnareddy31@gmail.com>
2021-05-25 10:04:44 -0700

    Initial commit

--- guice-benchmark/src/strategies/format/CSVFormatter.java
+++ guice-benchmark/src/strategies/format/CSVFormatter.java
@@ -15,6 +15,25 @@ public class CSVFormatter implements ReportFormatStrategy{
 	@Override
 	public void formatOutputStream(OutputStream stream, List<StatsObject> data) {
 		// TODO Auto-generated method stub
-		
+
+		response.setContentType("application/ms-excel"); // or you can use text/csv
+		response.setHeader("Content-Disposition", "attachment; filename=output.csv");
+		try {
+			// Write the header line
+			OutputStream out = response.getOutputStream();
+			String header = "EmployeeCode, Band\n";
+			out.write(header.getBytes());
+			// Write the content
+			String line=new String(EmployeeCode+","+Band+"\n");
+			out.write(line.toString().getBytes());
+			out.flush();
+			out.close();
+		}
+
+		catch (Exception e)
+		{
+			log.error(e);
+		}
+
 	}
 }
