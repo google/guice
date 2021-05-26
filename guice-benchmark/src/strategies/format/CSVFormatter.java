5eda43994f916da1b006ed8449761afbd5590872
Meghna Reddy <meghnareddy31@gmail.com>
2021-05-26 12:37:25 -0700

    Updated csv formatter

--- guice-benchmark/src/strategies/format/CSVFormatter.java
+++ guice-benchmark/src/strategies/format/CSVFormatter.java
@@ -2,7 +2,6 @@ package strategies.format;
 
 import java.io.OutputStream;
 import java.util.List;
-import com.opencsv.CSVWriter;
 import benchmark.StatsObject;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -20,37 +19,16 @@ public class CSVFormatter implements ReportFormatStrategy{
 	@Override
 	public void formatOutputStream(OutputStream stream, List<StatsObject> data) {
 
+		stream.write("Class Name, Average, Max, Min, P50, P90, P99");
+		for(int i=0; i< data.size(); i++) {
 
-		List<String[]> csvData = createCsvDataSimple();
+			String temp=data.get(i).getClassName()+","+data.get(i).getAverage()+","+data.get(i).getMax()
+					+","+data.get(i).getMin()+","+data.get(i).getP50()+","+data.get(i).getP90()+","+data.get(i).getP99()+"\n";
+			stream.write(temp);
 
-		// default all fields are enclosed in double quotes
-		// default separator is a comma
-		try ()
-		{
-			CSVWriter writer = new CSVWriter(new FileWriter("c:\\benchmark.csv"));
-			writer.writeAll(csvData);
-			writer.close();
+		}
+
 		}
 
 
-		private static List<String[]> createCsvDataSimple() {
-			String[] header = {"Average", "Max", "Min", "P50", "P90", "P99"};
-			List<String[]> list = new ArrayList<>();
-			list.add(header);
-
-			//loop through List<StatsObject> to store information in an array format as below
-
-			//like String[] record1 = {"1", "first name", "address 1", "11111"};
-			//like String[] record2 = {"2", "second name", "address 2", "22222"};
-			//list.add(each array)
-
-			//like list.add(record1);
-
-			return list;
-
-			//Source: https://mkyong.com/java/how-to-export-data-to-csv-file-java/
-			//additional: https://stackoverflow.com/questions/35749250/java-write-the-list-string-into-csv-file
-		}
-
-	}
 }
