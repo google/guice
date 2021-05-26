6b75c19ab08db2fe153e00682cc04ca723664d4d
Meghna Reddy <meghnareddy31@gmail.com>
2021-05-26 01:47:43 -0700

    How to add opencsv to "pom", and complete loop

--- guice-benchmark/src/strategies/format/CSVFormatter.java
+++ guice-benchmark/src/strategies/format/CSVFormatter.java
@@ -2,37 +2,54 @@ package strategies.format;
 
 import java.io.OutputStream;
 import java.util.List;
-
+import com.opencsv.CSVWriter;
 import benchmark.StatsObject;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 /**
  * 
- * @author Meghna
+ * @author Gucci
  *
  */
+
 public class CSVFormatter implements ReportFormatStrategy{
 
 	@Override
 	public void formatOutputStream(OutputStream stream, List<StatsObject> data) {
-		// TODO Auto-generated method stub
 
-		response.setContentType("application/ms-excel"); // or you can use text/csv
-		response.setHeader("Content-Disposition", "attachment; filename=output.csv");
-		try {
-			// Write the header line
-			OutputStream out = response.getOutputStream();
-			String header = "EmployeeCode, Band\n";
-			out.write(header.getBytes());
-			// Write the content
-			String line=new String(EmployeeCode+","+Band+"\n");
-			out.write(line.toString().getBytes());
-			out.flush();
-			out.close();
+
+		List<String[]> csvData = createCsvDataSimple();
+
+		// default all fields are enclosed in double quotes
+		// default separator is a comma
+		try ()
+		{
+			CSVWriter writer = new CSVWriter(new FileWriter("c:\\benchmark.csv"));
+			writer.writeAll(csvData);
+			writer.close();
 		}
 
-		catch (Exception e)
-		{
-			log.error(e);
+
+		private static List<String[]> createCsvDataSimple() {
+			String[] header = {"Average", "Max", "Min", "P50", "P90", "P99"};
+			List<String[]> list = new ArrayList<>();
+			list.add(header);
+
+			//loop through List<StatsObject> to store information in an array format as below
+
+			//like String[] record1 = {"1", "first name", "address 1", "11111"};
+			//like String[] record2 = {"2", "second name", "address 2", "22222"};
+			//list.add(each array)
+
+			//like list.add(record1);
+
+			return list;
+
+			//Source: https://mkyong.com/java/how-to-export-data-to-csv-file-java/
+			//additional: https://stackoverflow.com/questions/35749250/java-write-the-list-string-into-csv-file
 		}
 
 	}
--- guice-benchmark/src/strategies/format/HTMLFormatter.java
+++ guice-benchmark/src/strategies/format/HTMLFormatter.java
@@ -15,20 +15,6 @@ public class HTMLFormatter implements ReportFormatStrategy {
 	@Override
 	public void formatOutputStream(OutputStream stream, List<StatsObject> data) {
 		// TODO Auto-generated method stub
-		OutputStream clientOutput = client.getOutputStream();
-
-		// HTML headers
-		clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
-		clientOutput.write(("ContentType: text/html\r\n").getBytes());
-		clientOutput.write("\r\n".getBytes());
-
-		Scanner scanner = new Scanner(new File("htmlformat.html"));
-		String htmlString = scanner.useDelimiter("\\Z").next();
-		scanner.close();
-		clientOutput.write(htmlString.getBytes("UTF-8"));
-
-		clientOutput.write("\r\n\r\n".getBytes());
-		clientOutput.flush();
 
 
 	}
