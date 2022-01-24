d88223b75f1d3e0854e71a063801398dd3103f3f
Xiaoming Jia <xiaomingjia@google.com>
2022-01-24 14:21:29 -0800

    publish 5.1.0 docs.

---
+++ api-docs/5.1.0/api-diffs/guice-assistedinject/guice-assistedinject-4.2.3_guice-assistedinject-5.0.2-SNAPSHOT/guice-assistedinject-5.0.2-SNAPSHOT/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.google.inject.assistedinject"},{"l":"com.google.inject.assistedinject.internal"}]
---
+++ api-docs/5.1.0/api-diffs/guice-dagger-adapter/guice-dagger-adapter-4.2.3_guice-dagger-adapter-5.0.2-SNAPSHOT/guice-dagger-adapter-5.0.2-SNAPSHOT/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.google.inject.daggeradapter","l":"DaggerAdapter.Builder"},{"p":"com.google.inject.daggeradapter","l":"DaggerAdapter"}]
