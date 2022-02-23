713dfe9455faafb445e9207f790abd9352cbd635
Krzysztof Sierszeń <krzysztof.sierszen@digitalnewagency.com>
2022-02-23 09:30:46 +0100

    Added missing metadata

--- extensions/persist/test/com/google/inject/persist/utils/SuiteAndTestResource.java
+++ extensions/persist/test/com/google/inject/persist/utils/SuiteAndTestResource.java
@@ -1,9 +1,28 @@
+/*
+ * Copyright (C) 2010 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.inject.persist.utils;
 
 import org.junit.rules.ExternalResource;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
+/**
+ *
+ * @author Krzysztof Sierszeń (krzysztof.sierszen@digitalnewagency.com)
+ */
 public abstract class SuiteAndTestResource extends ExternalResource {
 
   public enum Lifecycle {
