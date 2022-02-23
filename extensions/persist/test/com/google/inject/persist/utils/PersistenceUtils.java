a9f20c859c45e55f1e0f9b967f068da9894d9645
Krzysztof Sierszeń <krzysztof.sierszen@digitalnewagency.com>
2022-02-23 09:30:46 +0100

    Added missing metadata

--- extensions/persist/test/com/google/inject/persist/utils/PersistenceUtils.java
+++ extensions/persist/test/com/google/inject/persist/utils/PersistenceUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2010 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.inject.persist.utils;
 
 import com.google.inject.Injector;
@@ -11,6 +26,10 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.TypedQuery;
 import org.hibernate.SessionFactory;
 
+/**
+ *
+ * @author Krzysztof Sierszeń (krzysztof.sierszen@digitalnewagency.com)
+ */
 public class PersistenceUtils {
 
   public static long successfulTransactionCount(EntityManagerFactory emf) {
