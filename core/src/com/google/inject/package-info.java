9678847695b64c8a9728fd84d312754fc00da3d6
Shane Kennedy <shane.kennedy19@gmail.com>
2023-04-18 13:13:08 -0700

    Remove references to Google Docs and instead point to the wiki when possible. Fixes #1556
    PiperOrigin-RevId: 525231598

--- core/src/com/google/inject/package-info.java
+++ core/src/com/google/inject/package-info.java
@@ -16,24 +16,24 @@
 
 /**
  * <i>Google Guice</i> (pronounced "juice") is an ultra-lightweight dependency injection framework.
- * Please refer to the Guice <a href="http://docs.google.com/Doc?id=dd2fhx4z_5df5hw8">User's
+ * Please refer to the Guice <a href="https://github.com/google/guice/wiki/Motivation">User's
  * Guide</a> for a gentle introduction.
  *
  * <p>The principal public APIs in this package are:
  *
  * <dl>
- * <dt>{@link com.google.inject.Inject}
- * <dd>The annotation you will use in your implementation classes to tell Guice where and how it
- *     should send in ("inject") the objects you depend on (your "dependencies").
- * <dt>{@link com.google.inject.Module}
- * <dd>The interface you will implement in order to specify "bindings" -- instructions for how Guice
- *     should handle injection -- for a particular set of interfaces.
- * <dt>{@link com.google.inject.Binder}
- * <dd>The object that Guice passes into your {@link com.google.inject.Module} to collect these
- *     bindings.
- * <dt>{@link com.google.inject.Provider}
- * <dd>The interface you will implement when you need to customize exactly how Guice creates
- *     instances for a particular binding.
+ *   <dt>{@link com.google.inject.Inject}
+ *   <dd>The annotation you will use in your implementation classes to tell Guice where and how it
+ *       should send in ("inject") the objects you depend on (your "dependencies").
+ *   <dt>{@link com.google.inject.Module}
+ *   <dd>The interface you will implement in order to specify "bindings" -- instructions for how
+ *       Guice should handle injection -- for a particular set of interfaces.
+ *   <dt>{@link com.google.inject.Binder}
+ *   <dd>The object that Guice passes into your {@link com.google.inject.Module} to collect these
+ *       bindings.
+ *   <dt>{@link com.google.inject.Provider}
+ *   <dd>The interface you will implement when you need to customize exactly how Guice creates
+ *       instances for a particular binding.
  * </dl>
  */
 package com.google.inject;
