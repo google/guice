d492cd8e9aa71c423ac036e83d9b37a33c273158
Kurt Alfred Kluever <kak@google.com>
2024-05-20 14:24:47 -0700

    Temporarily remove `@InlineMe` from `Modules.combine(Module)`.
    PiperOrigin-RevId: 635570248

--- core/src/com/google/inject/util/Modules.java
+++ core/src/com/google/inject/util/Modules.java
@@ -23,7 +23,6 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.google.errorprone.annotations.CheckReturnValue;
-import com.google.errorprone.annotations.InlineMe;
 import com.google.inject.AbstractModule;
 import com.google.inject.Binder;
 import com.google.inject.Binding;
@@ -126,7 +125,7 @@ public final class Modules {
   /**
    * @deprecated there's no need to "combine" one module; just install it directly.
    */
-  @InlineMe(replacement = "module")
+  // TODO(b/341757497): re-enable this inlining: @InlineMe(replacement = "module")
   @Deprecated
   public static Module combine(Module module) {
     return module;
